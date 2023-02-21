ec85ae81a9a33b9d9fb27be234c2436413068c87
Gerardo <geraz26@hotmail.com>
2023-02-21 14:02:01 -0600

    Actualizando dependencia

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getPokemonsPagination());
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="bg-slate-200">
